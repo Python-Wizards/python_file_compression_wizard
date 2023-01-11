38e90e39340e675efa64b0604107f3e70fb40d32
Angelo Kim Hui Lim <angelokimhui@gmail.com>
2023-01-11 18:02:03 +0800

    main.py: added necessary comments Make the program much more readable by providing comments to the code and its functions.

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 # basic file compression/decompression program using zipfile module
 # please take note that the program uses comments for now to execute specific def.
 
+# Import "zipfile" module from python
 import zipfile
 
+# compress_file definition/function
 def compress_file(file_name):
+
+    # Initialize the compression parameters
     with zipfile.ZipFile('test.zip', 'w') as zip:
+        # Compress based on the file name provided
         zip.write(file_name)
 
+# decompress_file definition/function
 def decompress_file(compressed_file_name):
+    
+    # Initialize the decompression parameters
     with zipfile.ZipFile(compressed_file_name, 'r') as zip:
+        # Decompress the file name provided to a folder named "extracted"
         zip.extractall(path="extracted")
 
 compress_file("testfile.txt")
