3c135d1e4019261705de2dd0523c16a0ee745f36
Maru <maru@maru.ga>
2023-01-17 16:47:29 +0800

    main.py: Added Tkinter module and created basic UI Only "Compress" and "Exit" buttons were created yet.

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 # basic file compression/decompression program using zipfile module
 # please take note that the program uses comments for now to execute specific def.
 
+# Declare important program variables
+prog_nm="CompressorWiz"
+prog_ver="0.1alpha"
+
+# Import front-end framework
+import tkinter as gui
 # Import "zipfile" module from python
 import zipfile
 
@@ -18,6 +24,16 @@ def decompress_file(compressed_filenm):
     with zipfile.ZipFile(compressed_filenm, 'r') as zip:
         # Decompress the file name provided to a folder named "extracted"
         zip.extractall(path="extracted")
+
+window = gui.Tk()
+window.title(prog_nm+"-("+prog_ver+")")
+header = gui.Label(window, text=prog_nm+"-"+prog_ver, height=5, font="Helvetica 20 bold")
+header.pack()
+button_extract = gui.Button(window, text = "Compress", command = lambda: compress_file("testfile.txt"))
+button_extract.pack()
+exit_button = gui.Button(window, text='Exit', command=window.destroy)
+exit_button.pack()
+window.mainloop()
 
 #compress_file("testfile.txt")
 #decompress_file("test.zip")
